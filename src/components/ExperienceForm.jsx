17ffe49006656439948fccefd9d82690b4372514
Brendan Lee <brendanjlee5@gmail.com>
2024-03-16 13:50:59 +0900

    add expereince

--- src/components/ExperienceForm.jsx
+++ src/components/ExperienceForm.jsx
@@ -1,41 +1,47 @@
+import { useState } from 'react';
 import '../styles/formStyle.css'
 
 function ExprienceForm() {
+  const [expInfo, setExpInfo] = useState({expName: '', expLoc: '', expStartDate: '', expEndDate: '', jobTitle: '', jobDesc: ''});
+
+  function handleChange(e) {
+    setExpInfo({...expInfo, [e.target.id]: e.target.value});
+  }
+
   return (
     <div className='userFormContainer'>
       <h3>Experience</h3>
       <form className='userForm'>
-        <label htmlFor="expInput">
+        <label>
           Company Name
-          <input type="text" id='expInput' name='expInput' placeholder='Apple' required/>
+          <input value={expInfo.expName} id='expName' onChange={handleChange} />
         </label>
 
-        <label htmlFor="expLocInput">
-          location
-          <input type="text" id='expLocInput' name='expLocInput' placeholder='Apple' required/>
+        <label>
+          Company Location
+          <input value={expInfo.expLoc} id='expLoc' onChange={handleChange} />
         </label>
 
-        <label htmlFor="expStartDateInput">
+        <label>
           Start Date
-          <input type="date" id='expStartDateInput' name='expStartDateInput' required/>
+          <input type='date' value={expInfo.expStartDate} id='expStartDate' onChange={handleChange} />
         </label>
 
-        <label htmlFor="expEndDateInput">
-          End Date (leave empty if current working)
-          <input type="date" id='expEndDateInput' name='expEndDateInput'/>
-        </label> 
+        <label>
+          End Date
+          <input type='date' value={expInfo.expEndDate} id='ExpEndDate' onChange={handleChange} />
+        </label>
 
-        <label htmlFor="jobTitleInput">
+        <label>
           Job Title
-          <input type="text" id='jobTitleInput' name='jobTitleInput' placeholder='Software Engineer' required/>
+          <input value={expInfo.jobTitle} id='jobTitle' onChange={handleChange} />
         </label>
 
-        <label htmlFor="jobDescInput">
+        <label>
           Job Description
-          <textarea id="jobDescInput" name='jobDescInput' cols="30" rows="5"></textarea>
+          <textarea name="jobDesc" id="jobDesc" cols="30" rows="5" onChange={handleChange}></textarea>
         </label>
 
-        <button type='submit'>Submit</button>
       </form>
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import InfoForm from './components/InfoForm'
 import EduForm from './components/EduForm'
+import ExprienceForm from './components/ExperienceForm'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <InfoForm />
     <EduForm />
+    <ExprienceForm />
   </React.StrictMode>,
 )
