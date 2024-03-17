cdc51e166e9fad05925dee46a84d41450397ddba
Brendan <brendanjlee5@gmail.com>
2024-03-18 06:39:23 +0900

    Update ExpContainer.jsx
    Clean up comments

--- src/components/ExpContainer.jsx
+++ src/components/ExpContainer.jsx
@@ -5,54 +5,10 @@ import '../styles/formStyle.css'
 
 function ExpContainer({expList, handleChange, addForm, delForm}) {
   const [formIsOpen, setFormIsOpen] = useState(true);
-  // const [expList, setExpList] = useState([
-  //   {
-  //     id: uuidv4(),
-  //     expName: 'Boeing',
-  //     expLoc: 'Seattle',
-  //     expStartDate: '',
-  //     expEndDate: '',
-  //     jobTitle: 'Software Engineer',
-  //     jobDesc: 'Make planes go beep boop'
-  //   }
-  // ]);
 
   function toggleForm() {
     setFormIsOpen(!formIsOpen);
   }
-
-  // function addForm() {
-  //   setExpList([
-  //     ...expList,
-  //     {
-  //       id: uuidv4(),
-  //       expName: '',
-  //       expLoc: '',
-  //       expStartDate: '',
-  //       expEndDate: '',
-  //       jobTitle: '',
-  //       jobDesc: ''
-  //     }
-  //   ])
-  // }
-
-  // function delForm() {
-  //   setExpList(expList.slice(0, -1));
-  // }
-
-  // function handleChange(e, expId) {
-  //   const index = expList.findIndex(exp => exp.id === expId);
-  //   if (index === -1) return;
-
-  //   // create a shallow copy of new list, update the element in the new list and set
-  //   const newList = [...expList];
-  //   newList[index] = {
-  //     ...newList[index],
-  //     [e.target.id]: e.target.value
-  //   };
-
-  //   setExpList(newList);
-  // }
 
   if (!formIsOpen) {
     return (
